5642c114ec6155ba29c34cc5110ca7d015a6b747
songfq24 <songfq24@gmail.com>
2018-11-29 21:17:48 -0800

    add black out mask

---
+++ HearthStone/Assets/Scripts/BlackMask.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlackMask : MonoBehaviour {
+
+    public static BlackMask _instance;
+
+    private void Awake()
+    {
+        _instance = this;
+        this.gameObject.SetActive(false);
+    }
+
+    public void Show()
+    {
+        this.gameObject.SetActive(true);
+    }
+
+    public void Hide()
+    {
+        this.gameObject.SetActive(false);
+    }
+}
